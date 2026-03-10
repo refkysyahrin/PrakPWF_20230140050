789a243a8fa231d1ed5bb5dcf7c2d9d83b09a245
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-03-10 16:38:11 +0700

    Tugas 3

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name',
+        'qty',
+        'price',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
